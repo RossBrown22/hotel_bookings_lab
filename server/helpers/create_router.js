1e4049369aef15f2803bc6042482cbca06e884b0
Adam Kidd <adamkidd3@gmail.com>
2022-01-12 11:39:44 +0000

    crud routes

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -2,23 +2,83 @@ const express = require('express');
 const ObjectID = require('mongodb').ObjectID;
 
 const createRouter = (collection) => {
-    
+
     const router = express.Router();
 
     //GET
     router.get('/', (req, res) => {
         collection.find().toArray()
-        .then(docs => res.json(docs))
-        .catch((err) => {
-            console.error(err);
-            res.status(500);
-            res.json({status: 500, error: err})
-        });
+            .then(docs => res.json(docs))
+            .catch((err) => {
+                console.error(err);
+                res.status(500);
+                res.json({ status: 500, error: err })
+            });
+    });
+
+    //SHOW - GET ONE BY ID
+    router.get('/:id', (req, res) => {
+        const id = req.params.id;
+        collection.findOne({ _id: ObjectID(id) })
+            .then((doc) => res.json(doc))
+            .catch((err) => {
+                console.error(err)
+                res.status(500)
+                res.json({
+                    'status': 500,
+                    'error': err
+                })
+            });
     });
 
     // CREATE
+    router.post('/', (req, res) => {
+        collection.insertOne(req.body)
+            .then((result) => res.json(result.ops[0]))
+            .catch((err) => {
+                console.error(err)
+                res.status(500)
+                res.json({
+                    'status': 500,
+                    'error': err
+                })
+            });
+    });
 
-    return router
-}
+    //DESTROY
+    router.delete('/:id', (req, res) => {
+        collection.deleteOne({ _id: ObjectID(req.params.id) })
+            .then((result) => res.json(result))
+            .catch((err) => {
+                console.error(err)
+                res.status(500)
+                res.json({
+                    'status': 500,
+                    'error': err
+                })
+            });
+    });
+
+    //UPDATE
+    router.put('/:id', (req, res) => {
+        const id = req.params.id;
+        const updatedData = req.body;
+        collection.updateOne(
+            { _id: ObjectID(id) },
+            { $set: updatedData }
+        )
+            .then((result) => res.json(result))
+            .catch((err) => {
+                console.error(err)
+                res.status(500)
+                res.json({
+                    'status': 500,
+                    'error': err
+                })
+            });
+    });
+
+    return router;
+};
 
 module.exports = createRouter;
